6f9e6eea6bcd8459bcfd629228a94c7a113de76f
Fouzan.p <147602953+fouzzann@users.noreply.github.com>
2024-05-02 14:42:46 +0530

    added

--- dart_application_d_s/bin/week_3/hey.dart
+++ dart_application_d_s/bin/week_3/hey.dart
@@ -1,129 +1,38 @@
-class Node{
-  var value;
-  Node? left;
-  Node? right;
-  Node(this.value);
+heapify(List<int> arr, int n, int i){
+  int largest = i;
+   int left = 2*i+1;
+   int right = 2*i+2;
+
+   if(left<n&&arr[left]>arr[largest]){
+    largest =left;
+   }
+   if(right<n&&arr[right]>arr[largest]){
+    largest = right;
+   }
+   if(largest!=i){
+    int temp = arr[i];
+    arr[i] = arr[largest];
+    arr[largest] = temp;
+    heapify(arr, n, largest);
+   }
 }
 
-class BineryTree{
-  Node? root;
-  insert(int value){
-    root = insertval(root , value);
-  }
-  Node? insertval(Node? root, int value){
-    if(root == null){
-      return Node(value);
-    }
-    if(value<root.value){ 
-      root.left = insertval(root.left, value);
-    }else if(value>root.value){
-      root.right = insertval(root.right, value);
-    }
-    return root;
+heapSort(List<int> arr){
+  int n= arr.length;
+  for(int i=n~/2-1;i>=0;i--){
+    heapify(arr, n,i );
   }
 
-  bool isBST(){
-    int? prev;
-    bool isBSTval(Node? node){
-      if(node == null) return true;
-      if(!isBSTval(node.left)) return false;
-      if(prev!=null&& prev!>=node.value) return false;
-      prev = node.value;
-      return isBSTval(node.right);
-    }
-    return isBSTval(root);
+  for(int i=n-1;i>=0;i--){
+    int temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    heapify(arr, i, 0);
   }
-
-  dis(Node? node){
-    if(node!=null){
-      dis(node.left);
-      print(node.value);
-      dis(node.right);
-    }
-  }
-
-  delete(int value){
-    root = deleteval(root,value);
-  }
-  Node? deleteval( Node? root, int value){
-    if(root== null){
-      return root;
-    }
-    if(value<root.value){
-      root.left = deleteval(root.left, value);
-    }else if( value>root.value){
-      root.right = deleteval(root.right, value);
-    }else{
-      if(root.left == null){
-        return deleteval(root.right, value);
-      }else if(value>root.value){
-        return deleteval(root.left, value);
-      }
-
-    }
-    return root;
-  }
-
-  search( int value){
-    return searchval(root,value);
-  }
-  Node? searchval(Node? node, int value){
-    if(node == null|| node.value == value){
-      return node;
-    }
-    if(value<node.value){
-      return searchval(node.left, value);
-    }else if( value> node.value){
-      return searchval(node.right, value);
-    }
-  }
-
-  smallest(Node? node){
-    while(node!.left!= null){
-      node = node.left;
-    }
-    return node.value;
-  }
-  largest(Node? node){
-    while(node!.right!= null){
-      node = node.right;
-    }
-    return node.value;
-  }
-
-find2ndlargest(Node? node){
-  if(node!.right== null&& node.left!= null){
-    return largest(node.left);
-  }
-  if(node.right!= null&& node.right!.left == null&& node.right!.right== null){
-    return node.value;
-
-  }
-  return find2ndlargest(node.right);
-}
-find2ndsmallest(Node? node){
-  if(node!.left == null&& node.right!= null){
-    return smallest(node.right);
-  }
-  if(node.right!= null&& node.right!.left== null&& node.right!.right == null){
-    return node.value;
-  }
-  return find2ndsmallest(node.right);
-}
 }
 
 void main(){
-  BineryTree node = BineryTree();
-  node.insert(1);
-  node.insert(2);
-  bool isBSt = node.isBST();
-  print('is this BST ? $isBSt');
-  node.delete(2);
-  Node searchval = node.search(1);
-  if(searchval!=null){
-    print('vale is gound ${searchval.value}');
-  }else{
-    print('no value found');
-  }
-  node.dis(node.root);
+  List<int> arr = [11,66,1,4,3,2];
+  heapSort(arr);
+  print(arr);
 }
